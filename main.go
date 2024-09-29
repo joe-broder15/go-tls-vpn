358653c412ce104a38630b3f05442950605079f3
joe-broder15 <joe.broder15@gmail.com>
2024-09-28 23:57:27 -0400

    finished boilerplate tls server

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ func main() {
 	serverPtr := flag.Bool("s", false, "run as a server")
 	pubkeyPtr := flag.String("pubkey", "server.crt", "run as a server")
 	privkeyPtr := flag.String("privkey", "server.key", "run as a server")
+	portPtr := flag.String("p", "8888", "server port")
+	// ipPtr := flag.String("p", "127.0.0.1", "ip address of server")
+
 	flag.Parse()
 
 	// determine whether to run the client or server
 	if *serverPtr {
-		server.Start(*pubkeyPtr, *privkeyPtr)
+		server.Start(*pubkeyPtr, *privkeyPtr, *portPtr)
 	} else {
 		fmt.Println("Run as client")
 	}
--- server/server.go
+++ server/server.go
@@ -3,15 +3,44 @@ package server
 import (
 	"crypto/tls"
 	"fmt"
+	"log"
+	"net"
 )
 
-func Start(certFile string, keyFile string) {
+func Start(certFile string, keyFile string, port string) {
 	fmt.Println("Starting server...")
 
-	_, err := tls.LoadX509KeyPair(certFile, keyFile)
+	// load the x509 Key Pair
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		log.Fatal(err)
 	}
-	// config := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	// create the tls configuration
+	config := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	// start the listener and set up a defer to close it
+	listener, err := tls.Listen("tcp", ":"+port, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	// listen for incoming connections
+	for {
+		// accept connections and hand them off to a new goroutine
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go func(c net.Conn) {
+
+			// send some data to the connected client
+			conn.Write([]byte("hello from the server over TLS\n"))
+
+		}(conn)
+	}
+
 }
