86ad4c4a53d1d507f6dec77a4316bca840aa0f3f
joe-broder15 <joe.broder15@gmail.com>
2024-09-28 23:13:53 -0400

    added options for certs and updated readme

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 func main() {
 	// set the command line flags
 	serverPtr := flag.Bool("s", false, "run as a server")
+	pubkeyPtr := flag.String("pubkey", "server.crt", "run as a server")
+	privkeyPtr := flag.String("privkey", "server.key", "run as a server")
 	flag.Parse()
 
 	// determine whether to run the client or server
 	if *serverPtr {
-		fmt.Println(server.ServerHello())
+		server.Start(*pubkeyPtr, *privkeyPtr)
 	} else {
 		fmt.Println("Run as client")
 	}
--- server/server.go
+++ server/server.go
@@ -1,5 +1,17 @@
 package server
 
-func ServerHello() string {
-	return "Hello Server"
+import (
+	"crypto/tls"
+	"fmt"
+)
+
+func Start(certFile string, keyFile string) {
+	fmt.Println("Starting server...")
+
+	_, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// config := &tls.Config{Certificates: []tls.Certificate{cert}}
 }
